463fdfd9bcb9f7fb1dafc8bc2b4a84e71dba7cc0
Foxyz24 <feliperamirez2004@gmail.com>
2023-11-01 16:35:29 -0400

    Added classes and started working on the matrix

--- Matrix.java
+++ Matrix.java
@@ -1,2 +1,35 @@
 public class Matrix {
+
+  int size;
+  final String RESET = "\u001B[0m";
+  final String YELLOW = "\u001B[33m";
+
+  public int[][] matrix(int size) {
+    int[][] matrix = new int[size][size];
+
+    return matrix;
+  }
+
+  public void printMatrix(int[][] matrix, int size){
+    int color = size;
+    for(int row = 0; row < size; row++){
+      for(int coloumn = 0; coloumn < size; coloumn++){
+        if(coloumn == color){
+          System.out.print(YELLOW + "0  " + RESET);
+        }else{
+          System.out.print("0  ");
+        }
+      }
+      System.out.println();
+    }
+  }
+
+  public void populateMatrix(int[][] matrix){
+  }
+
+  public void swapMatrix(int[][] matrix){
+  }
+
+
+
 }
--- RunMatrix.java
+++ RunMatrix.java
@@ -2,5 +2,23 @@ import java.util.Scanner;
 
 public class RunMatrix {
   public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    String input;
+    int size = 0;
+
+    System.out.print("Please enter a number bigger than 0");
+
+    try{
+      if(size > 0){
+        size = scan.nextInt();
+      }else{
+        System.out.println("You entered an number lower than 1, please try again");
+      }
+    } catch (Exception e){
+      System.out.println("You entered something that was not a number, please run again and try again");
+    }
+
+
+
   }
 }
