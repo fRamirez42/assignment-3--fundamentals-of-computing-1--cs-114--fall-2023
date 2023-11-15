8df5721d14ffb43f5ccab0ace91245e53f849904
Foxyz24 <feliperamirez2004@gmail.com>
2023-11-15 16:30:06 -0500

    variable name

--- Matrix.java
+++ Matrix.java
@@ -38,12 +38,12 @@ public class Matrix {
   }
 
   public void flipMatrix(){
-    int middle = inputSize;
+    int diagonal = inputSize;
     for(int row = 0; row < matrix.length - 1; row++){
-      for(int column = 0; column < middle-1; column++){ //Updates to end the loop before the middle
+      for(int column = 0; column < diagonal-1; column++){ //Updates to end the loop before the diagonal
         swap(row, column, (inputSize-1) - row, (inputSize-1) - column);
       }
-      middle--;
+      diagonal--;
     }
   }
 
